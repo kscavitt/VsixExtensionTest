6d1e02807d738526aa31b29922b8b73b6edd64bd
Netmonster <kscavitt@protonmail.com>
2022-10-19 11:23:14 -0500

    changed command to inhert from BaseDICommand per doc. removed packageRestore from nuget.config file

--- src/TestVSIXProject1/Commands/MyToolWindowCommand.cs
+++ src/TestVSIXProject1/Commands/MyToolWindowCommand.cs
@@ -1,8 +1,16 @@
-﻿namespace TestVSIXProject1
+﻿using Community.VisualStudio.Toolkit.DependencyInjection;
+using Community.VisualStudio.Toolkit.DependencyInjection.Core;
+
+namespace TestVSIXProject1
 {
     [Command(PackageIds.MyCommand)]
-    internal sealed class MyToolWindowCommand : BaseCommand<MyToolWindowCommand>
+    internal sealed class MyToolWindowCommand : BaseDICommand
     {
+        public MyToolWindowCommand(DIToolkitPackage package) : base(package)
+        {
+
+        }
+
         protected override Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             return MyToolWindow.ShowAsync();
